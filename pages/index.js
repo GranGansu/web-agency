ba9b0176f7c941603b0b4fd247212ac6ed7f2753
DESKTOP-O393MCR\Gansu <asztamfaterg01@educantabria.es>
2022-11-09 00:20:09 +0100

    primer proyecto 2

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Mi primer proyecto NEXT
+          Mi primer proyecto NEXT2
         </h1>
 
         <p className={styles.description}>
