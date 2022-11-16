a5782222818c24f5047b41389edb31e39a430716
DESKTOP-O393MCR\Gansu <asztamfaterg01@educantabria.es>
2022-11-16 11:55:44 +0100

    16/11

---
+++ pages/Que.js
@@ -0,0 +1,19 @@
+export default function Que() {
+    return (
+        <div>
+            <h1>Qué es una web?</h1>
+            <div className="hidden">
+                <h4>Caso real:</h4>
+                <p><b>Intención:</b> Quieres ver el menú, y elegir entre las opciones veganas</p>
+                <p><b>Realidad:</b> Después de 2 minutos, sigues descargándote un PDF con la letra demasiado pequeña, y sin poder filtrar las opciones veganas.</p>
+            </div>
+            <p>En Internet no gana el más original, gana el que transmita mejor su mensaje</p>
+
+            <h2>Qué hacemos y qué no hacemos?</h2>
+            <p>NO</p>
+            <ul>
+                <li>No entregamos webs copy paste</li>
+            </ul>
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
+import { Fredoka } from '@next/font/google'
 import '../styles/globals.css'
-
+const comforta = Fredoka({ preload: true, subsets: ['latin'] })
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const comfota = comforta.className
+  return <Component {...pageProps} fuente={comfota} />
 }
 
 export default MyApp
