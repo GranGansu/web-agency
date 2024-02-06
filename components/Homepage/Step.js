573fcd718e7d8e1849fe2b4dfa176e1c995508d5
Alan Sztamfater <ganzonet@gmail.com>
2024-02-06 17:21:51 +0100

    before upload

--- components/Homepage/Step.js
+++ components/Homepage/Step.js
@@ -7,7 +7,7 @@ export default function Step({title, iconID}) {
     <div className='max-w-2xl m-auto'>
     <div className='bg-white grid grid-cols-2 text-2xl py-12 mb-2'>
       <div className="flex items-center justify-center w-full">{title}</div>
-      <div className="flex items-center justify-center w-full text-3xl">{icons[iconID]}</div>
+     {/*  <div className="flex items-center justify-center w-full text-3xl">{icons[iconID]}</div> */}
     </div></div>
   );
 }
