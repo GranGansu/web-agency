ac31c234ab5321e448121515291b3bba0fd8eddd
Alan Sztamfater <ganzonet@gmail.com>
2024-02-06 17:29:15 +0100

    before upload

--- components/Homepage/Step.js
+++ components/Homepage/Step.js
@@ -2,7 +2,7 @@ import BuildCircleIcon from '@mui/icons-material/BuildCircle';
 import CurrencyYenIcon from '@mui/icons-material/CurrencyYen';
 import AddCardIcon from '@mui/icons-material/AddCard';
 export default function Step({title, iconID}) {
-  const icons =  [<BuildCircleIcon/>,<CurrencyYenIcon/>,<AddCardIcon/>]
+  /* const icons =  [<BuildCircleIcon/>,<CurrencyYenIcon/>,<AddCardIcon/>] */
   return (
     <div className='max-w-2xl m-auto'>
     <div className='bg-white grid grid-cols-2 text-2xl py-12 mb-2'>
