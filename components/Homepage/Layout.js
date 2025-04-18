99c64cccdd1845fcbad4b8ac7f9e0a9b3c5504c4
Alan Sztamfater <ganzonet@gmail.com>
2025-04-18 11:45:15 +0200

    add:GA4

--- components/Homepage/Layout.js
+++ components/Homepage/Layout.js
@@ -1,12 +1,10 @@
 import { Footer, Nav } from '../layout';
-import Main from './Main';
 
 export default function Layout({ children, fuente }) {
   return (
     <div className={`${fuente}`}>
-{/*       <Nav /> */}
       {children}
-      <Footer  />
+      <Footer />
     </div>
   );
 }
