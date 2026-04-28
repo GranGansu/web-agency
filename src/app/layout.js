68944c425aa866cf3d5a748c279ea9949965963e
Alan Sztamfater <ganzonet@gmail.com>
2026-04-28 15:43:57 +0200

    add:clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,18 @@
 import { Lexend_Deca, Barlow_Condensed, Manrope, Jost, Space_Grotesk, Roboto, Kumbh_Sans } from 'next/font/google';
 const lexend = Lexend_Deca({ preload: true, subsets: ['latin'], weight: '400', variable: '--font-lexend' });
 const barlow = Barlow_Condensed({ preload: true, subsets: ['latin'], weight: '400', variable: '--font-barlow' });
-const manrope = Manrope({ preload: true, subsets: ['latin'], weight: '400', variable: '--font-manrope', });
+const manrope = Manrope({ preload: true, subsets: ['latin'], weight: '400', variable: '--font-manrope' });
 const jost = Jost({ preload: true, subsets: ['latin'], weight: '400', variable: '--font-jost' });
-const roboto = Roboto({ preload: true, subsets: ['latin'], weight: '400', variable: '--font-roboto', });
-const kumb = Kumbh_Sans({ preload: true, subsets: ['latin'], weight: ['100','400','800'], variable: '--font-kumb', });
+const roboto = Roboto({ preload: true, subsets: ['latin'], weight: '400', variable: '--font-roboto' });
+const kumb = Kumbh_Sans({ preload: true, subsets: ['latin'], weight: ['100', '400', '800'], variable: '--font-kumb' });
 const space_Grotesk = Space_Grotesk({ preload: true, subsets: ['latin'], weight: '400' });
 import './styles/globals.css';
 import Footer from './components/layout/Footer';
 import Volver from './components/atoms/Volver';
 import Navigation from './components/layout/Navigation';
 import { TooltipProvider } from '@radix-ui/react-tooltip';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/next';
+import Script from 'next/script';
 export const metadata = {
   metadataBase: new URL('https://superbeam.es'),
 };
@@ -27,7 +28,16 @@ export default function RootLayout({ children }) {
         <Volver />
         <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
         <Footer />
-         <Analytics />
+        <Analytics />
+        <Script id='clarity-script' strategy='afterInteractive'>
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wis5vsdz22");
+          `}
+        </Script>
       </body>
     </html>
   );
